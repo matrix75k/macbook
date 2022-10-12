4f1999d2735ffbe25adb30fc74a7e1280bf4d32d
matrix75k <113307664+matrix75k@users.noreply.github.com>
2022-10-12 00:57:22 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,163 @@
+// j.s
+// 12   3 app   4 oop
+
+// use 
+
+// web 
+// mobile apps    ionic   flutter  react native 
+// games           playcanvas 
+// server          client side     server side 
+
+// desktop apps  .......
+
+///////////////////////////////////////////////////////////////
+
+// ............
+//   head 
+//  at the end of body 
+//  ex j.s file 
+//////////////////////////////////////////////////////////////
+
+//  4 -- 
+
+// document.write("islam") 
+
+
+// document.getElementById("result").innerHTML = "islam mohamed"
+
+// document.getElementById("btn").onclick = function (){
+//     document.getElementById("result").innerHTML = "web development"
+// }
+
+
+// console.log("ahmed")
+
+// var x  = 90;
+
+// x = 250
+
+// console.log(x)
+
+
+// alert("do you have an acc ?");
+
+//////////////////////////////////////////////////////////////////////////
+
+// statement     
+
+//  syntax  
+
+//  comment 
+
+// 
+
+/* 
+
+
+*/
+///////////////////////////////////////////////////////////////////////////
+
+// variables 
+
+// var  num1 = 50 ;
+
+// var  num2 = 90 ;
+
+// console.log(num1+num2)
+
+/////////////////////////////////////////////////////////////////////////
+
+// data types  [ number     string      boolean     array     object      null      undefined ]
+
+// var  x = 9;
+
+// x = 50;
+// console.log(x)
+
+// console.log(typeof  x )
+
+///////////////////////////////////////////////////////////////////
+
+// var  x =  "8252" ;
+
+// console.log(x)
+
+// console.log(typeof x )
+
+// ////////////////////////////////////////////////////////////////
+
+// var  x = true ;
+
+// console.log(x)
+
+// console.log(typeof x )
+
+///////////////////////////////////////////////////////////////
+
+// array   
+
+// object 
+
+
+// var  x  = [  "ahmed" , "hoda" , 22  , "tasneem" , "merna" , true , "marawan"  ]
+
+// console.log(x)
+
+// console.log(typeof x )
+
+// console.log(x[0])
+
+// console.log (typeof x[3])
+
+// console.log (typeof x[2])
+
+// console.log (x[5])
+
+// /////////////////////////////////////////////////////////////
+
+
+// var  per1  =  {
+//     name : "merna" ,
+//     city : "mansoura",
+//     number : "010000000000" ,
+// }
+
+//   console.log(per1)
+
+//   console.log(per1.city)
+
+//   console.log(per1.number)
+
+//   console.log(typeof per1.name)
+
+////////////////////////////////////////////////////////////
+
+//   var x  = 40;
+
+//    x = null ;
+
+//    console.log(typeof  x ) 
+
+//    ////////////////////////////////////////////////////
+
+
+var x  ;
+
+var x  = null ;
+
+console.log(x)
+
+
+    
+    
+   
+      
+     
+
+
+
+
+
+
+
+
